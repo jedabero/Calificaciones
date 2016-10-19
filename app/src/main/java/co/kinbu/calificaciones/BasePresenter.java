fa3fb0f5ac8e085794c070807848848f51c54c84
jedabero <jedabero@hotmail.com>
2016-10-18 21:53:58 -0500

    Added base presenter and view interfaces

---
+++ app/src/main/java/co/kinbu/calificaciones/BasePresenter.java
@@ -0,0 +1,12 @@
+package co.kinbu.calificaciones;
+
+/**
+ *
+ * Created by jedabero on 18/10/16.
+ */
+
+interface BasePresenter {
+
+    void start();
+
+}
---
+++ app/src/main/java/co/kinbu/calificaciones/BaseView.java
@@ -0,0 +1,12 @@
+package co.kinbu.calificaciones;
+
+/**
+ *
+ * Created by jedabero on 18/10/16.
+ */
+
+interface BaseView<T> {
+
+    void setPresenter(T presenter);
+
+}
